6eb7b73d17a89fe66ca51864b79e49fadb99bdd3
lsl233 <ahands233@gmail.com>
2024-08-21 16:13:15 +0800

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ToasterProvider } from "@/components/providers/toaster-provider";
@@ -8,8 +8,15 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "微习惯",
   description: "先有一个习惯",
-  viewport: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+
 };
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+}
 
 export default function RootLayout({
   children,
